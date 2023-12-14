287a8f868274be615bf79e1745bad19f3e9a5e4e
MiroslavAmbros <Miroslav.ambros@gmail.com>
2023-12-14 20:30:08 +0100

    Update edunovawp3.sql

--- edunovawp3.sql
+++ edunovawp3.sql
@@ -1,8 +1,9 @@
 use master;
+go
 drop database if exists edunovawp3;
 go
 -- ovo je komentar
-create database edunovawp3;
+create database edunovawp3 collate Croatian_CI_AS;
 go
 --drop database edunovawp3;
 --use master
@@ -10,24 +11,22 @@ use edunovawp3;
 
 create table smjerovi(
 sifra int not null primary key identity(1,1),
-naziv varchar(50)not null,
-trajanje int null, --null se ne piše. Ako ne piše not null onda se podrazumijeva
-cijena decimal(18,2), --iako ništa ne piše je null  
+naziv varchar(50) not null,
+trajanje int null, --null se ne piše. Ako ne piše not null onda se podrazumjeva null
+cijena decimal(18,2), -- iako ništa ne piše je null
 vaucer bit
 );
 
 create table grupe(
 
-	sifra			int 		not null primary key identity(1,1),
-	naziv			varchar(5)  not null,
-	smjer			int 		not null,
+	sifra			int			not null primary key identity(1,1),
+	naziv			varchar(5)	not null,
+	smjer			int			not null,
 	datumpocetka	datetime,
 	maxpolaznika	int			not null,
 	predavac		int
 
 );
-
-ovo je dio koji je promjenjen
 
 
 create table polaznici(
@@ -36,14 +35,14 @@ ime varchar(50) not null,
 prezime varchar(50) not null,
 email varchar(100),
 oib char(11),
-brojugovora (10)
+brojugovora varchar(10)
 );
 
 create table predavaci(
 sifra int not null primary key identity(1,1),
-ime varchar(50)not null,
-prezime varchar(50)not null,
-email varchar(100)not null,
+ime varchar(50) not null,
+prezime varchar(50) not null,
+email varchar(100) not null,
 oib char(11),
 iban varchar(50)
 );
@@ -57,12 +56,13 @@ grupa int not null,
 polaznik int not null
 );
 
---kreiranje vanjskih ključeva
-alter table grupe add foreign key(smjer) references smjerovi(sifra);
+-- kreiranje vanjskih ključeva
+alter table grupe add foreign key (smjer) references smjerovi(sifra);
 alter table grupe add foreign key (predavac) references predavaci(sifra);
 
 alter table clanovi add foreign key (grupa) references grupe(sifra);
-alter table clanovi add foreign key ( polaznik) references polaznici( sifra);
+alter table clanovi add foreign key (polaznik) references polaznici(sifra);
+
 
 
 
